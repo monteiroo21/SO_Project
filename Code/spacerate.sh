a29f77a8f54ba9c0fa5aa4558ab2a4200e3626ed
jpapinto <jpapinto@ua.pt>
2023-11-11 12:05:41 +0000

    Spacerate.sh: Funciona com e sem espaços; Desconta apenas o size do subdiretório direto (senão estaria a descontar a mesma coisa várias vezes); Já coloquei as opções de visualização -a, -r e -l (copiei do outro script) ; Ver testes feitos em notas.txt para explicar no relatório. Acho que está tudo a funcionar corretamente.

--- Code/spacerate.sh
+++ Code/spacerate.sh
@@ -21,17 +21,14 @@ while getopts ":ral:" opt; do
             ;;
         l)
             l="$OPTARG"
-            if [ "$l" -lt 0 ]; then
-                echo "Insert a number of lines that is bigger than zero!"
+            if ! [[ "$l" =~ ^[1-9]+$ ]]; then
+                echo "You have to give a number of lines greater then zero (an integer)!"
                 exit 1;
             fi
             ;;
+
         \?)
             echo "Opção inválida: -$OPTARG" >&2
-            exit 1
-            ;;
-        :)
-            echo "A opção -$OPTARG requer um argumento." >&2
             exit 1
             ;;
     esac
@@ -54,57 +51,155 @@ declare -A fileA_dict
 declare -A fileB_dict
 declare -A rate_dict
 
+realSize(){
+    local directory="$1"
+    local size=0
+
+    if [[ "$2" == "A" ]]; then
+      for key in "${!fileA_dict[@]}"; do
+          if [[ "$key" == "$directory" || ("$key" == "$directory"/* && "$key" != "$directory"/*/*) ]]; then
+              if [[ "$key" == "$directory" ]]; then # Se for o próprio diretório
+                size=$((size + fileA_dict["$key"]))
+              else
+                  size=$((size - fileA_dict["$key"])) # A chave é um subdiretório direto do diretório pai. Caso contrário estaria a descontar mais do que uma vez (se considerasse apenas "$directory"/*).
+              fi
+          fi
+      done
+    else
+      for key in "${!fileB_dict[@]}"; do
+          if [[ "$key" == "$directory" || ("$key" == "$directory"/* && "$key" != "$directory"/*/*) ]]; then
+              if [[ "$key" == "$directory" ]]; then # Se for o próprio diretório
+                size=$((size + fileB_dict["$key"]))
+              else
+                  size=$((size - fileB_dict["$key"])) # A chave é um subdiretório direto do diretório pai. Caso contrário estaria a descontar mais do que uma vez (se considerasse apenas "$directory"/*).
+              fi
+          fi
+      done
+    fi
+
+    echo "$size"
+}
+
+
 # Função que calcula a evolução do espaço ocupado
 calculate_size_evolution(){
-    while IFS= read -r lineB; do
-        if [[ "$lineB" == "SIZE NAME"* ]]; then
+    while read -r size path; do
+        if [[ "$size" == "SIZE" ]]; then
             continue
         else
-            folderB=$(echo "$lineB" | awk '{print $2}')
-            sizeB=$(echo "$lineB" | awk '{print $1}')
-            fileB_dict["$folderB"]="$sizeB"
+            fileA_dict["$path"]="$size"
+
         fi
-    done < "$fileB"
+    done <<< "$(cat "$fileA" | awk '{ printf $1; for (i=2; i<=NF; i++) printf " %s", $i; print "" }')"
 
+    while read -r size path; do
+            if [[ "$size" == "SIZE" ]]; then
+                continue
+            else
+                fileB_dict["$path"]="$size"
 
-    while IFS= read -r lineA; do
-        if [[ "$lineA" == "SIZE NAME"* ]]; then
-            continue
-        else
-            folderA=$(echo "$lineA" | awk '{print $2}')
-            sizeA=$(echo "$lineA" | awk '{print $1}')
-            fileA_dict["$folderA"]="$sizeA";
-        fi
-    done < "$fileA"
+            fi
+    done <<< "$(cat "$fileB" | awk '{ printf $1; for (i=2; i<=NF; i++) printf " %s", $i; print "" }')"
 
-
+    # Loop para calcular as diferenças
     for key in "${!fileA_dict[@]}"; do
-
         if [[ -n ${fileB_dict[$key]+x} ]]; then
-            sizeB="${fileB_dict[$key]}"
-            sizeA="${fileA_dict[$key]}"
-            size_rate=$((sizeA - sizeB))
+            # echo "$key"
+            sizeB=$(realSize "$key")
+            sizeA=$(realSize "$key" "A")
+            # echo "$sizeA" - "$sizeB"
+            size_rate=$((sizeA-sizeB))
             rate_dict["$key"]=$size_rate
         else
-            size_rate="${fileA_dict[$key]}"
+            sizeA=$(realSize "$key" "A")
             key_new="$key NEW"
-            rate_dict["$key_new"]="$size_rate";
+            rate_dict["$key_new"]=$sizeA
         fi
-
     done
 
     for key in "${!fileB_dict[@]}"; do
         if [[ ! -n ${fileA_dict[$key]+x} ]]; then
-            sizeB="${fileB_dict[$key]}"
+            sizeB=$(realSize "$key")
             key_removed="$key REMOVED"
-            rate_dict["$key_removed"]="$sizeB";
+            rate_dict["$key_removed"]=$sizeB
         fi
     done
+
 }
 
 calculate_size_evolution
 printHeader
+#
+#for key in "${!rate_dict[@]}"; do
+#    echo "${rate_dict[$key]} $key"
+#done
 
-for key in "${!rate_dict[@]}"; do
-    echo "${rate_dict[$key]} $key"
-done
+#for key in "${!fileA_dict[@]}"; do
+#    echo "${fileA_dict[$key]} $key"
+#done
+
+##directory="Teste 2/aula2"
+##directory="Teste 2/abc"
+#directory="Teste 2"
+#
+#echo "Nome do diretorio cujo tamanho real quero calcular: $directory"
+#echo " "
+#size=0;
+#for key in "${!fileA_dict[@]}"; do
+#
+#          echo "${fileA_dict[$key]} $key"
+#          if [[ "$key" == "$directory" || ("$key" == "$directory"/* && "$key" != "$directory"/*/*) ]]; then # A chave é um subdiretório direto do diretório pai
+#              if [[ "$key" == "$directory" ]]; then # Se for o próprio diretório
+#                echo "Eu (key) sou igual ao diretório passado como argumento"
+#                echo " "
+#                size=$((size + fileA_dict["$key"]))
+#              else
+#                echo "Eu (key) sou um subdiretorio direto do diretorio passado como argumento."
+#                echo ""
+#                size=$((size - fileA_dict["$key"]))
+#              fi
+#          else
+#                echo "Não sou o diretorio passado como argumento nem um subdiretorio direto dele."
+#                echo ""
+#          fi
+#
+#done
+#
+#echo "Size real de $directory: $size"
+
+# Função para visualizar a ocupação do espaço como pretendido
+display(){
+    if [ "$a" -eq 0 ] && [ "$r" -eq 0 ] && [ "$l" -eq 0 ]; then
+        for key in "${!rate_dict[@]}"; do
+            echo "${rate_dict[$key]} $key"
+        done | sort -r -n -k1
+    elif [ "$a" -eq 1 ]; then
+        if [ "$r" -eq 1 ]; then
+            echo "You can only choose one option between -a and -r. Try again!"
+        elif [ "$l" -gt 0 ]; then
+            for key in "${!rate_dict[@]}"; do
+                echo "${rate_dict[$key]} $key"
+            done | sort -k2 | head -n $l
+        else
+            for key in "${!rate_dict[@]}"; do
+                echo "${rate_dict[$key]} $key"
+            done | sort -k2
+        fi
+    elif [ "$r" -eq 1 ]; then
+        if [ "$l" -gt 0 ]; then
+            for key in "${!rate_dict[@]}"; do
+                echo "${rate_dict[$key]} $key"
+            done | sort -n -k1 | head -n $l
+        else
+            for key in "${!rate_dict[@]}"; do
+                echo "${rate_dict[$key]} $key"
+            done | sort -n -k1
+        fi
+    elif [ "$l" -gt 0 ]; then
+        for key in "${!rate_dict[@]}"; do
+            echo "${rate_dict[$key]} $key"
+        done | sort -r -n -k1 | head -n $l
+    fi
+}
+
+display
